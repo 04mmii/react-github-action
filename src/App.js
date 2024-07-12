2aaf6d92e71b690ac9dc1bea3f80eb211ebbda80
04mmii <you@example.com>
2024-07-12 13:06:35 +0900

    👍

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  
-  const [count, setCunt ] = useState(0);
-  const [ disabled, setDisabled ] = useState(false);
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h3 data-testid="counter">{ count }</h3>
+        <h3 data-testid="counter">{count}</h3>
         <button 
           data-testid="minus-button" 
-          onClick={() => setCunt((prev) => prev - 1)}
+          onClick={() => setCount((prev) => prev - 1)}
           disabled={disabled}
         >-</button>
         <button 
           data-testid="plus-button" 
-          onClick={() => setCunt((prev) => prev + 1)}
+          onClick={() => setCount((prev) => prev + 1)}
           disabled={disabled}
         >+</button>
       </header>
       <div>
         <button 
           style={{ backgroundColor: "blue" }}
-          data-testid="on/off-button"
+          data-testid="on-off-button"
           onClick={() => setDisabled(prev => !prev)}
         >on/off
         </button>
